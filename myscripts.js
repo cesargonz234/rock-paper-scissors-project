c363e4b813ed936d0fe1877fee80ed02f6f90ddc
Cesar <cesargonz7183@gmail.com>
2023-07-13 20:43:05 -0700

    case insensative

--- myscripts.js
+++ myscripts.js
@@ -15,5 +15,18 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    playerSelection = playerSelection.toLowerCase()
+    if (playerSelection === computerChoice){
+        console.log("It's a tie.")
+    }
+    else if (playerSelection === "rock" && computerChoice === "paper") {
+         console.log("You lose. Paper beats rock")
+         
+    }
+    else if (playerSelection === "rock" && computerChoice ==="scissor") {
+        console.log("You win. Rock beats scissor.")
+    }
 }
+let playerSelection = "rock";
+const computerChoice = "paper";
+console.log(playRound(playerSelection, computerChoice))
