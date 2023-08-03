03cdb2fd26aa2bb1cd3972ccc95668b221400eff
Cesar <cesargonz7183@gmail.com>
2023-08-02 19:17:16 -0700

    finished project.

--- myscripts.js
+++ myscripts.js
@@ -13,31 +13,79 @@ function getComputerChoice(){
         return computerChoice
     }
 }
+function getPlayerChoice() {
+    let ask = prompt("play 5 rounds, see who wins", "rock, paper, scissor ?")
+    while (ask == null) {
+        ask = prompt("play 5 rounds, see who wins", "rock, paper, scissor ?")
+    }
+    ask = ask.toLowerCase()
+    return ask;
+}
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
+
+function playRound(playerSelection, computerChoice) {
+    
     if (playerSelection === computerChoice){
-        console.log("It's a tie.")
+        return "It's a tie.";
     }
     else if (playerSelection === "rock" && computerChoice === "paper") {
-         console.log("You lose. Paper beats rock")
+        return "You lose. Paper beats rock";
          
     }
     else if (playerSelection === "rock" && computerChoice ==="scissor") {
-        console.log("You win. Rock beats scissor.")
+        return "You win. Rock beats scissor.";
     }
     else if (playerSelection === "paper" && computerChoice === "rock") {
-        console.log("You win, paper beats rock")
+        return "You win, paper beats rock";
     }
     else if (playerSelection === "paper" && computerChoice === "scissor"){
-        console.log("You lose, scissor beats rock")
+        return "You lose, scissor beats paper";
     }
     else if (playerSelection === "scissor" && computerChoice === "paper"){
-        console.log("You win, scissor beats paper")
+        return "You win, scissor beats paper";
     }
     else if (playerSelection === "scissor" && computerChoice === "rock")
-    console.log("You lose, rock beats scissor")
+        return "You lose, rock beats scissor";
+    else {
+        return "please type correctly"
+    }
 }
-let playerSelection = "rock";
-const computerChoice = "rock";
-console.log(playRound(playerSelection, computerChoice))
+let playerCount = 0;
+let compCount = 0;
+function winCounter(playerSelection, computerChoice){
+    if (playerSelection === computerChoice){
+        return 0;
+    }
+    else if  ((playerSelection === "paper" && computerChoice === "rock" ) ||
+        (playerSelection ==="scissor" && computerChoice ===" scissor")||
+        (playerSelection === "rock" && computerChoice ==="paper")
+)
+    {
+       return playerCount = playerCount + 1;
+    }
+    else {
+        return compCount = compCount + 1;
+    }
+    
+}
+function winDecider(playerCount, compCount){
+    if (playerCount === compCount){
+        return "its a draw";
+    }
+    else if (playerCount > compCount){
+        return "you Win!";
+    }
+    else {
+        return "You lose, the robots have taken over!!";
+    }
+}
+
+function game(){
+    for(i = 0; i < 5; i++){
+        playerSelection = getPlayerChoice()
+        let computerChoice = getComputerChoice()
+        console.log(playRound(playerSelection, computerChoice))
+    }
+    console.log(winDecider(playerCount, compCount))
+}
+game()
